9dbc16889e61cf164dba397371925a16c3e723ae
Sokaris Mykerinos <chepseskaf@gmail.com>
2022-11-14 13:06:39 +0100

    remove QML loader, add play/stop button, enable build from CMake, add comment, add logs

--- tests/examples/qt/qmlsink-multisink/main.cpp
+++ tests/examples/qt/qmlsink-multisink/main.cpp
@@ -2,27 +2,36 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 
+#include <iostream>
+
 #include <gst/gst.h>
 
 int
 main (int argc, char *argv[])
 {
-  gst_init (&argc, &argv);
+  setenv("GST_TRACERS", "leaks", false);
+  setenv("GST_DEBUG", "*:2,GST_TRACER:7,videoitem:5", false);
 
+  gst_init (&argc, &argv);
+  gchar* version = gst_version_string();
+  std::cerr << version << std::endl;
+  g_free(version);
+
+  int exit_status;
+  {
   QGuiApplication app (argc, argv);
   QQmlApplicationEngine engine;
 
   /* make sure that plugin was loaded */
   GstElement *qmlglsink = gst_element_factory_make ("qmlglsink", NULL);
   g_assert (qmlglsink);
+  gst_object_unref (qmlglsink);
 
   /* anything supported by videotestsrc */
   QStringList patterns (
-      {
-      "smpte", "ball", "spokes", "gamut"});
+      {"smpte"});
 
-  engine.rootContext ()->setContextProperty ("patterns",
-      QVariant::fromValue (patterns));
+  engine.rootContext ()->setContextProperty ("patterns", QVariant::fromValue (patterns));
 
   QObject::connect (&engine, &QQmlEngine::quit, [&] {
         gst_object_unref (qmlglsink);
@@ -31,5 +40,11 @@ main (int argc, char *argv[])
 
   engine.load (QUrl (QStringLiteral ("qrc:///main.qml")));
 
-  return app.exec ();
+  exit_status = app.exec ();
+  }
+
+
+  gst_deinit();
+
+  return exit_status;
 }
